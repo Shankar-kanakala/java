093395c5557657ea7df6cea3a6b9dff1d3ffea81
Shankar Kanakala <37285410+Shankar-kanakala@users.noreply.github.com>
2019-08-12 16:58:07 +0530

    Create SieveofEratosthenes.java

---
+++ SieveofEratosthenes.java
@@ -0,0 +1,24 @@
+//Program to generate prime numbers using SieveofEratosthenes algorithm.
+import java.util.*;
+public class SieveofEratosthenes
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        List<Integer> primelist = new ArrayList<Integer>();
+        for(int i = 2; i <= n; i++) {
+            primelist.add(i);
+        }
+        for(int k = 0; k < primelist.size();k++) {
+            for(int l = k + 1; l < primelist.size(); l++) {
+                if(primelist.get(l) % primelist.get(k) == 0)
+                {
+                    primelist.remove(l);
+                }
+                    
+            }
+        }
+        System.out.println(primelist);
+    }
+    
+}
